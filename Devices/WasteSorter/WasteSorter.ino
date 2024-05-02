c299f6bc7804daa19125cb6adb50977339837907
Jovan Jokić <102528502+jokicjovan@users.noreply.github.com>
2024-05-02 20:01:43 +0200

    Servos functionality added
    Servo motors move depending on state change.

--- Devices/WasteSorter/WasteSorter.ino
+++ Devices/WasteSorter/WasteSorter.ino
@@ -1,9 +1,39 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <ESP32Servo.h>
 
+// Enums
+typedef enum ItemType{
+  RECYCABLE, NONRECYCABLE, UNDEFINED
+};
+
+// PINS
+static const int recycableServoPin = 26;
+static const int nonrecycableServoPin = 13;
+
+// Variables;
+Servo recycableServo;
+Servo nonrecycableServo;
+ItemType itemType;
+
+void setup() {
+  Serial.begin(9600);
+  recycableServo.attach(recycableServoPin);
+  nonrecycableServo.attach(nonrecycableServoPin);
+  recycableServo.write(80);
+  nonrecycableServo.write(80);
+  itemType = UNDEFINED;
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  if(itemType == RECYCABLE){
+    recycableServo.write(140);
+    delay(5000);
+    itemType = UNDEFINED;
+    recycableServo.write(80);
+  }
+  else if(itemType == NONRECYCABLE){
+    nonrecycableServo.write(30);
+    delay(5000);
+    itemType = UNDEFINED;
+    nonrecycableServo.write(80);
+  delay(5000);
 }
