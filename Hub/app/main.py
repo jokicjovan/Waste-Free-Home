eba3ca121f10a159b2e7a8b43a91b4873d665bfb
Jovan Jokić <102528502+jokicjovan@users.noreply.github.com>
2024-07-07 17:50:29 +0200

    Deprecation fix

--- Hub/app/main.py
+++ Hub/app/main.py
@@ -1,3 +1,5 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 
 from app.entities.schemas import UserCredentials
@@ -5,21 +7,23 @@ from app.core.config import settings
 from app.core.utils import update_env_file, get_jwt
 from app.core.mqtt_handler import mqtt_client, on_connect, on_message
 
-app = FastAPI()
 
-
-@app.on_event("startup")
-async def startup_event():
+@asynccontextmanager
+async def lifespan(app: FastAPI):
     get_jwt()
     mqtt_client.on_connect = on_connect
     mqtt_client.on_message = on_message
     mqtt_client.connect(settings.mqtt_broker_address, settings.mqtt_broker_port, 60)
     mqtt_client.loop_start()
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
 
 
 @app.put("/change-credentials")
 async def update_credentials(credentials: UserCredentials):
-    settings.user_credentials = credentials.email
+    settings.user_email = credentials.email
     settings.user_password = credentials.password
     update_env_file("user_email", credentials.email)
     update_env_file("user_password", credentials.password)
