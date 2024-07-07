49bc52e110bb126f09b2f5e710c7562624469ae0
Jovan Jokić <102528502+jokicjovan@users.noreply.github.com>
2024-07-07 20:37:55 +0200

    QR code generating implemented

--- Server/app/services/base_device_service.py
+++ Server/app/services/base_device_service.py
@@ -1,10 +1,13 @@
 from uuid import UUID
-
-from pydantic import ValidationError
 from sqlalchemy.orm import Session
 
-from app.entities import schemas, models, time_series_models
+from app.entities import schemas, models
 from app.entities.enums import DeviceType
+
+model_mapping = {
+    DeviceType.THERMOMETER: models.Thermometer,
+    DeviceType.WASTE_SORTER: models.WasteSorter
+}
 
 
 def get_device(db: Session, device_id: UUID):
@@ -19,16 +22,20 @@ def get_user_devices(db: Session, user_id: UUID, skip: int = 0, limit: int = 100
     return db.query(models.BaseDevice).filter(models.BaseDevice.owner_id == user_id).offset(skip).limit(limit).all()
 
 
-def create_user_device(db: Session, device: schemas.DeviceCreate, user_id: UUID):
-    if device.type == DeviceType.THERMOMETER:
-        model_class = models.Thermometer
-    elif device.type == DeviceType.WASTE_SORTER:
-        model_class = models.WasteSorter
-    else:
-        return None
-    db_device = model_class(owner_id=user_id, **device.model_dump(exclude={'type'}))
-    db.add(db_device)
+def link_device_to_user(db: Session, device_id: UUID, user_id: UUID):
+    db_device = get_device(db, device_id)
+    db_device.owner_id = user_id
     db.commit()
     db.refresh(db_device)
     return db_device
 
+
+def create_device(db: Session, device_schema: schemas.DeviceCreate):
+    model_class = model_mapping.get(device_schema.type)
+    if not model_class:
+        return None
+    db_device = model_class(**device_schema.model_dump(exclude={'type'}))
+    db.add(db_device)
+    db.commit()
+    db.refresh(db_device)
+    return db_device
